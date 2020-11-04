1037229f4e74962909c245b4c613837cccfd691d
gendreaua <fel16012@byui.edu>
2020-11-03 21:11:49 -0800

    discount update

---
+++ cit160/js/discount.js
@@ -0,0 +1,19 @@
+function sales() {
+
+    //Input
+        let dayOfWeek = new Date().getDay();
+        let subtotal = parseFloat(document.getElementById("subtotal").value);
+
+    
+    // Processing
+    
+        if (subtotal > 50 && (dayOfWeek == 2 || dayOfWeek == 3)) {
+            discount = subtotal - (subtotal * .10);
+    }
+        else {
+        discount = subtotal;
+    }
+    //Output
+    
+        document.getElementById('output').innerHTML = "You Owe: $" + (discount + (discount * .06)).toFixed(2)
+    }
