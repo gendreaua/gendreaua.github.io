5ae68931b9682292a6010901eef79fab1fe6ec12
gendreaua <fel16012@byui.edu>
2020-11-11 21:29:10 -0800

    w08 loops, sum odds

---
+++ cit160/js/sumOdds.js
@@ -0,0 +1,17 @@
+function sumOdds() {
+
+    //Input
+        let userNumber = parseInt(document.getElementById("userNumber").value);
+        let sum = 0;
+        let count;
+
+    // Processing   
+        for (let i = 1; i < userNumber; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }  
+    
+    //Output   
+        document.getElementById('output').innerHTML = "The sum of the odd numbers within the number " + userNumber + " is " + sum + "."
+}
