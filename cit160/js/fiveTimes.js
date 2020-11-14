45f34aa94e87dc514b165ae65e455abe4c11f824
gendreaua <fel16012@byui.edu>
2020-11-14 10:52:36 -0800

    five times updated

--- cit160/js/fiveTimes.js
+++ cit160/js/fiveTimes.js
@@ -2,7 +2,7 @@ function fiveTimes() {
     //Input
     let output = "";
     //Processing loop the times tables twelve times
-    for (let i = 5; i <= 12; i++) {
+    for (let i = 1; i <= 12; i++) {
         output += "5 X " + i + " = " + i * 5 +'<br>';
     }
     //Output
