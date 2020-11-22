4a827d256583d85da9b866a79a274c9d0e38d246
gendreaua <fel16012@byui.edu>
2020-11-22 12:30:53 -0800

    final update windchill

--- cit160/js/windChill.js
+++ cit160/js/windChill.js
@@ -1,17 +1,26 @@
-function sumOdds() {
+function doInputOutput() {
+    //Input to get the information from the user for temperature and windspeed
+        let tempF = parseInt(document.getElementById("temperature").value);
+        let windspeed = parseInt(document.getElementById("windspeed").value);
+        let w = "";
+    // Processing to pull the calculation from the windChill function and calculate if temperature and windspeed fit the requirements
+        if (tempF <= 50 && windspeed >= 3) {
+            w = windChill(tempF, windspeed).toFixed(2) + "&deg" + " Fahrenheit";
+        }
+        else if (tempF > 50) {
+            w = "The temperature must be above 50 &deg Fahrenheit to calculate wind chill."
+        }
+        else {
+            w = "The windspeed must be above 3 miles per hour to calculate wind chill."
+        }   
+    //Output the windchill factor or an error message for what the user is missing
+        document.getElementById('output').innerHTML = w
+}
 
-    //Input
-        let userNumber = parseInt(document.getElementById("userNumber").value);
-        let sum = 0;
-        let count;
-
-    // Processing   
-        for (let i = 1; i <= userNumber; i++) {
-            if (i % 2 != 0) {
-                sum += i;
-            }
-        }  
-    
-    //Output   
-        document.getElementById('output').innerHTML = "The sum of the odd numbers within the number " + userNumber + " is " + sum + "."
+function windChill(tempF, windspeed) {
+    //Input - no input for this function
+    //Processing
+    w = 35.74 + (0.6215 * tempF) - (35.75 * windspeed ** 0.16) + (0.4275 * tempF * windspeed ** 0.16);
+    return w;
+    //Output - no output for this function
 }
